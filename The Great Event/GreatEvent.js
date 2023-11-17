ac6d79c0001cba2645e4537126a1730232a366b4
Scholar <scholar@paradigm.local>
2023-11-17 12:07:24 -0700

    funny silly functions yippee

--- The Great Event/GreatEvent.js
+++ The Great Event/GreatEvent.js
@@ -24,12 +24,17 @@ document.getElementById("wheel").addEventListener("wheel", function () {
     document.getElementById("wheel").style.backgroundColor = "purple";
     document.getElementById("wheel").style.height = "500px";
     document.getElementById("wheel").style.width = "50px";
+    document.getElementById("wheel").innerHTML = ":(";
+    document.getElementById("wheel").style.color = "white";
 });
 
 document.getElementById("theGreatDeleter").addEventListener("contextmenu", function () {
     document.getElementById("theGreatDeleter").style.backgroundColor = "crimson";
     document.getElementById("color").style.opacity = "0%";
     document.getElementById("wheel").style.opacity = "0%";
+    document.getElementById("scaredFellow").style.opacity = "0%";
+    document.getElementById("squash").style.opacity = "0%";
+    document.getElementById("sillydiv").style.opacity = "0%";
     document.getElementById("greeting").style.opacity = "0%";
     document.getElementById("theGreatDeleter").innerHTML = "MWAAHAHAHAHAHHAHHAHAHHAHHA >:)";
 });
@@ -42,11 +47,27 @@ document.getElementById("scaredFellow").addEventListener("copy", function () {
     
 });
 
-document.getElementById("squash").addEventListener("", function () {
+document.getElementById("squash").addEventListener("mousedown", function () {
     document.getElementById("squash").style.backgroundColor = "brown";
     document.getElementById("squash").style.height = "60px";
     document.getElementById("squash").style.width = "700px";
     document.getElementById("squash").innerHTML = "dang";
+    
+});
+
+document.getElementById("squash").addEventListener("mouseup", function () {
+    document.getElementById("squash").style.backgroundColor = "orangered";
+    document.getElementById("squash").style.height = "90px";
+    document.getElementById("squash").style.width = "500px";
+    document.getElementById("squash").innerHTML = "I got hospital :(";
+    
+});
+
+document.getElementById("letterHater").addEventListener("keydown", function () {
+    document.getElementById("sillydiv").style.backgroundColor = "blue";
+    document.getElementById("sillydiv").innerHTML = "NOO 💀";
+    document.getElementById("sillydiv").style.fontSize = "200px";
+
     
 });
 
