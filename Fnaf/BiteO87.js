e709e5b3dfb3d413fafb3d8e79a0c3828ed8d578
BigSkouz <143551966+BigSkouz@users.noreply.github.com>
2023-12-08 17:46:39 -0700

    Update BiteO87.js

--- Fnaf/BiteO87.js
+++ Fnaf/BiteO87.js
@@ -4,3 +4,11 @@ function fnafQuiz () {
 
     document.getElementById("submitMessage").innerHTML = `Hello ${name}, thank you for submitting the quiz. Also ${character} is the silliest character (imagine)`;
 }
+
+document.getElementById("funnybutton").addEventListener("mouseover", function () {
+    document.getElementById("funnybutton").style.backgroundColor = "red";
+});
+
+document.getElementById("funnybutton").addEventListener("mouseout", function () {
+    document.getElementById("funnybutton").style.backgroundColor = "white";
+});
